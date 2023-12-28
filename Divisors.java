34f84376f20560337cc816e131b2bf0efc5032ac
אריאל גס <arielgess@gmail.com>
2023-12-28 08:12:01 +0200

    add divisors program solution

--- Divisors.java
+++ Divisors.java
@@ -1,8 +1,13 @@
 /**
- *  Gets a command-line argument (int), and prints all the divisors of the given number.
+ * Gets a command-line argument (int), and prints all the divisors of the given number.
  */
 public class Divisors {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
+    public static void main(String[] args) {
+        int number = Integer.parseInt(args[0]);
+        for (int i = 1; i <= number; i++) {
+			if (number % i == 0){
+				System.out.println(i);
+			}
+        }
+    }
 }
