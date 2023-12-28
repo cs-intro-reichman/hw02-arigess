c8cd4cab30c336a9b8f5263ddcc4325b743e994d
אריאל גס <arielgess@gmail.com>
2023-12-28 13:27:42 +0200

    add a program that prints a damka board

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,8 +1,18 @@
 /**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
+ * Gets a command-line argument n (int), and prints an n-by-n damka board.
  */
 public class DamkaBoard {
-	public static void main(String[] args) {
-		//// Put your code here
-	}
+    public static void main(String[] args) {
+        int numberOfLines = Integer.parseInt(args[0]);
+        for (int i = 0; i < numberOfLines; i++) {
+            for (int j = 0; j < numberOfLines; j++) {
+                if (i % 2 == 0) { //in even lines, we will start with *
+                    System.out.print("* ");
+                } else { // in odd lines, we will start with a space
+                    System.out.print(" *");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
