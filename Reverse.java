27317b1be16ee2752774a32b4449eb831ceea6fa
אריאל גס <arielgess@gmail.com>
2023-12-28 08:30:38 +0200

    add a program that print a reversed string and its middle character

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,12 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+		String phrase = args[0];
+		for (int i=0; i< phrase.length(); i++){
+			System.out.print(phrase.charAt(phrase.length() - 1 - i));
+		}
+		System.out.println();
+		char middleChar = phrase.charAt((int)Math.ceil((double)phrase.length()/2) - 1);
+		System.out.printf("The middle character is %c", middleChar);
 	}
 }
