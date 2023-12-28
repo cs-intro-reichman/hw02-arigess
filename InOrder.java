5172bf8d80666a1987d5269b70d599f3dad074a1
אריאל גס <arielgess@gmail.com>
2023-12-28 09:17:58 +0200

    add a program that generates a sequence of increasing nubmers between 0 to 10

--- InOrder.java
+++ InOrder.java
@@ -4,6 +4,13 @@
  */
 public class InOrder {
 	public static void main (String[] args) {
-		//// Write your code here
+		int previousNumber = 0;
+		int randomNumber = (int) Math.floor(Math.random() * (10));
+		do {
+			System.out.println(randomNumber);
+			previousNumber = randomNumber;
+			randomNumber = (int) Math.floor(Math.random() * (10));
+		} while (previousNumber < 10 && randomNumber >= previousNumber);
+
 	}
 }
