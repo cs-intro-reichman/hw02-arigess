d5848ef412a73f5b92e924156b860eecb1f63bf9
אריאל גס <arielgess@gmail.com>
2023-12-28 13:21:12 +0200

    add program that prints whether a number is perfect

--- Perfect.java
+++ Perfect.java
@@ -1,8 +1,19 @@
 /**
- *  Gets a command-line argument (int), and chekcs if the given number is perfect.
+ * Gets a command-line argument (int), and chekcs if the given number is perfect.
  */
 public class Perfect {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
+    public static void main(String[] args) {
+        int number = Integer.parseInt(args[0]);
+        int divisorsSum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) { // if i is a divisor of number
+                divisorsSum += i;
+            }
+        }
+        if (divisorsSum == number) {
+            System.out.printf("%d is a perfect number", number);
+        } else {
+            System.out.printf("%d is not a perfect number", number);
+        }
+    }
 }
