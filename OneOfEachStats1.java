7900c87935449bdd33fe67a36d77cb5c2e13067a
אריאל גס <arielgess@gmail.com>
2023-12-30 17:56:06 +0200

    beautify code

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -6,15 +6,12 @@
  */
 public class OneOfEachStats1 {
     public static void main(String[] args) {
-        //int experimentsCount = Integer.parseInt(args[0]);
-        int experimentsCount = 5;
+        int T = Integer.parseInt(args[0]);
         int twoChildren = 0, threeChildren = 0, fourPlusChildren = 0;
         int totalChildren = 0;
-        for (int i = 0; i < experimentsCount; i++) {
+        for (int i = 0; i < T; i++) {
             //an experiment
             boolean isBoy = false, isGirl = false;
-            isBoy = false;
-            isGirl = false;
             int childrenCount = 0;
             while (!isBoy || !isGirl) { // keep having babies until you have both a boy and a girl
                 double randomValue = Math.random();
@@ -35,12 +32,12 @@ public class OneOfEachStats1 {
                 fourPlusChildren++;
             }
         }
-        System.out.println("Average: " + (double)totalChildren / experimentsCount + " children to get at least one of each gender.");
+        System.out.println("Average: " + (double) totalChildren / T + " children to get at least one of each gender.");
         System.out.println("Number of families with 2 children: " + twoChildren);
         System.out.println("Number of families with 3 children: " + threeChildren);
         System.out.println("Number of families with 4 or more children: " + fourPlusChildren);
         String mostCommonChildren = "4 or more.";
-        if (twoChildren >= threeChildren && twoChildren >= fourPlusChildren){
+        if (twoChildren >= threeChildren && twoChildren >= fourPlusChildren) {
             mostCommonChildren = "2.";
         } else if (threeChildren >= twoChildren && threeChildren >= fourPlusChildren) {
             mostCommonChildren = "3.";
