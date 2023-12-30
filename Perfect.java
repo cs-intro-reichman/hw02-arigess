714496689ae6ffe3db73a0c17a840ff0b2e93632
אריאל גס <arielgess@gmail.com>
2023-12-30 17:12:48 +0200

    fix the print when the number is perfect

--- Perfect.java
+++ Perfect.java
@@ -4,14 +4,16 @@
 public class Perfect {
     public static void main(String[] args) {
         int number = Integer.parseInt(args[0]);
-        int divisorsSum = 0;
-        for (int i = 1; i < number; i++) {
+        String perfectString = String.format("%d is a perfect number since %d = 1", number, number);
+        int divisorsSum = 1; // 1 is always a divisor of any integer
+        for (int i = 2; i < number; i++) {
             if (number % i == 0) { // if i is a divisor of number
                 divisorsSum += i;
+                perfectString += " + " + i;
             }
         }
         if (divisorsSum == number) {
-            System.out.printf("%d is a perfect number", number);
+            System.out.printf(perfectString);
         } else {
             System.out.printf("%d is not a perfect number", number);
         }
